188f613bff1964004f5e0b4b5357b9b1e83b47e4
Palkit Lohia <57593495+spookbite@users.noreply.github.com>
2021-07-30 02:09:48 +0530

    favicon hehe

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import streamlit.components.v1 as components
 
 #st.set_page_config(layout="wide")
-st.set_page_config(page_title='Kronos2.0', layout = 'wide')
+st.set_page_config(page_title='Kronos2.0',page_icon = 'favicon.ico',layout = 'wide')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 head = """<script async defer data-website-id="f7b646ab-c9b9-4995-bd4c-14e1b9590000" src="https://umami.mukul-mehta.in/umami.js"></script>"""
 components.html(head, height=0)
